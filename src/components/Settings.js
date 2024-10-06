b9c84aa5774ce289e92c8037057414bc79bc544a
Aeshe <syslichek@mail.ru>
2024-10-06 16:55:46 +0300

    добавлен компонент настроек

---
+++ src/components/Settings.js
@@ -0,0 +1,21 @@
+import MenuCardBox from "./cards/MenuCardBox";
+
+const Settings = () => {
+  return (
+    <main>
+      <section className="title">
+        <div className="container title-container">
+          <h2>Настройки</h2>
+        </div>
+      </section>
+
+      <section className="settings">
+        <div className="container"></div>
+      </section>
+
+      <MenuCardBox />
+    </main>
+  );
+};
+
+export default Settings;

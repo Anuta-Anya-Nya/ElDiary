7ef03706747812e89fe3fc6673be4a5b8f5fe353
Aeshe <syslichek@mail.ru>
2024-11-04 15:07:23 +0300

    добавлена загрузка учителей из БД

--- src/db/teachersDb.js
+++ src/db/teachersDb.js
@@ -1,6 +1,17 @@
 import shortid from "shortid";
 import { db } from "../firebase/firebase";
-import { setDoc, doc } from "firebase/firestore";
+import { setDoc, doc, getDocs, collection } from "firebase/firestore";
+
+export const getTeachersDB = async (userId) => {
+  const resp = await getDocs(collection(db, `users/${userId}/teachers/`));
+  const arr = resp.docs.reduce((obj, el) => {
+    obj[el.data().id] = el.data();
+    return obj;
+  }, {});
+  console.log(arr);
+
+  return arr;
+};
 
 export const addTeacherDb = async ({ userId, teacher }) => {
   const teacherId = shortid.generate();
--- src/store/slices/teachersSlice.js
+++ src/store/slices/teachersSlice.js
@@ -1,51 +1,67 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getTeachersDB } from "../../db/teachersDb";
+
+export const getTeachersThunk = createAsyncThunk(
+  "teachers/getTeachersThunk",
+  async (userId) => {
+    try {
+      const teachersList = await getTeachersDB(userId);
+      console.log(teachersList);
+      return { teachersList, loading: false };
+    } catch (er) {
+      console.log(er.code, er.message);
+      return { teachersList: {}, loading: false };
+    }
+  }
+);
 
 const teachersSlice = createSlice({
   name: "teachers",
   initialState: {
+    loading: true,
     teachersList: {
-      1: {
-        id: 1,
-        name: "Иванова Мария Михайловна",
-        tel: "(905)123-22-33",
-        birthdate: null,
-        teachingLessons: [1, 3],
-      },
-      2: {
-        id: 2,
-        name: "Петрова Лилия Григорьевна",
-        tel: "(905)111-22-33",
-        birthdate: null,
-        teachingLessons: [2],
-      },
-      3: {
-        id: 3,
-        name: "Сидорова Валентина Михайловна",
-        tel: "(905)123-22-33",
-        birthdate: null,
-        teachingLessons: [7],
-      },
-      4: {
-        id: 4,
-        name: "Матвеева Валентина Михайловна",
-        tel: "(905)123-22-33",
-        birthdate: null,
-        teachingLessons: [7],
-      },
-      5: {
-        id: 5,
-        name: "Валентинова Валентина Михайловна",
-        tel: "(905)123-22-33",
-        birthdate: null,
-        teachingLessons: [5],
-      },
-      6: {
-        id: 6,
-        name: "Михайлова Валентина Михайловна",
-        tel: "(905)123-22-33",
-        birthdate: null,
-        teachingLessons: [5],
-      },
+      // 1: {
+      //   id: 1,
+      //   name: "Иванова Мария Михайловна",
+      //   tel: "(905)123-22-33",
+      //   birthdate: null,
+      //   teachingLessons: ["4Cn4xJFFJ2"],
+      // },
+      // 2: {
+      //   id: 2,
+      //   name: "Петрова Лилия Григорьевна",
+      //   tel: "(905)111-22-33",
+      //   birthdate: null,
+      //   teachingLessons: ["4Cn4xJFFJ2"],
+      // },
+      // 3: {
+      //   id: 3,
+      //   name: "Сидорова Валентина Михайловна",
+      //   tel: "(905)123-22-33",
+      //   birthdate: null,
+      //   teachingLessons: ["4Cn4xJFFJ2"],
+      // },
+      // 4: {
+      //   id: 4,
+      //   name: "Матвеева Валентина Михайловна",
+      //   tel: "(905)123-22-33",
+      //   birthdate: null,
+      //   teachingLessons: ["4Cn4xJFFJ2"],
+      // },
+      // 5: {
+      //   id: 5,
+      //   name: "Валентинова Валентина Михайловна",
+      //   tel: "(905)123-22-33",
+      //   birthdate: null,
+      //   teachingLessons: ["4Cn4xJFFJ2"],
+      // },
+      // 6: {
+      //   id: 6,
+      //   name: "Михайлова Валентина Михайловна",
+      //   tel: "(905)123-22-33",
+      //   birthdate: null,
+      //   teachingLessons: ["4Cn4xJFFJ2"],
+      // },
     },
   },
   reducers: {
@@ -61,14 +77,11 @@ const teachersSlice = createSlice({
     },
   },
   //   редьюсеры для thunk функций
-  //   extraReducers: (builder) => {
-  //     builder.addCase(createUserThunk.fulfilled, (state, action) => {
-  //       return (state = action.payload);
-  //     });
-  //     builder.addCase(loginThunk.fulfilled, (state, action) => {
-  //       return (state = action.payload);
-  //     });
-  //   },
+  extraReducers: (builder) => {
+    builder.addCase(getTeachersThunk.fulfilled, (state, action) => {
+      return (state = action.payload);
+    });
+  },
 });
 export const { addTeacher } = teachersSlice.actions;
 export const teacherReducer = teachersSlice.reducer;

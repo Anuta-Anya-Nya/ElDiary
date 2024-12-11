adf8fab987b7f8ec3e5cf9b383cbdacf7e137618
Aeshe <syslichek@mail.ru>
2024-12-11 19:39:46 +0300

    поправлены стили в модалке с информацией об уроке

--- src/components/customModal/ModalLessonInfo.js
+++ src/components/customModal/ModalLessonInfo.js
@@ -1,38 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { updateDailyScheduleLessonThunk } from "../../store/slices/dailySchedulesSlice";
-import { openCloseModal } from "../../store/slices/contentSlice";
-import { findCurrentStudyYear } from "../../utils/services";
-import moment from "moment/min/moment-with-locales.min";
+import { openCloseModal, saveModalData } from "../../store/slices/contentSlice";
 
 export const ModalLessonInfo = () => {
   const modalData = useSelector((state) => state.content.openModal.modalData);
   const teacher = useSelector(
     (state) => state.teachers.teachersList[modalData.lesson.teacherId]
   );
-  console.log(teacher);
-  console.log(modalData);
   const dispatch = useDispatch();
-
   const toClose = () => {
     dispatch(openCloseModal({ lessonInfoModal: false }));
+    dispatch(saveModalData({}));
   };
 
-  // useEffect(() => {
-  //   if (modify) {
-  //     setRadioValue(String(modalData.grade));
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
   return (
     <div className="modal-content">
       <div className="modal-content-choice modal-content-choice-grade">
-        <div className="modal-content-choice-item">Урок: {modalData.title}</div>
         <div className="modal-content-choice-item">
-          Кабинет: {modalData.lesson.cabinet || "Не записан"}
+          <span>Урок:</span> {modalData.title}
         </div>
         <div className="modal-content-choice-item">
-          Учитель: {teacher ? teacher.name : "Не записан"}
+          <span>Кабинет: </span>
+          {modalData.lesson.cabinet || "Не записан"}
+        </div>
+        <div className="modal-content-choice-item">
+          <span>Учитель: </span>
+          {teacher ? teacher.name : "Не записан"}
         </div>
       </div>
 
